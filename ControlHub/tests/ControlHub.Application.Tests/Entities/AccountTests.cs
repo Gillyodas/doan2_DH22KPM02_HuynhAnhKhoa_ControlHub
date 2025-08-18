ee2593ace26826cb02c28e735ac13ab39542dd94
Gillyodas <gillyodaspendragon94@gmail.com>
2025-08-19 00:34:46 +0700

    test(domain): add unit tests for Account and User entities

---
+++ ControlHub/tests/ControlHub.Application.Tests/Entities/AccountTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ControlHub.Domain.Entities;
+using Xunit;
+
+namespace ControlHub.Domain.Tests.Entities
+{
+    public class AccountTests
+    {
+        [Fact]
+        public void Constructor_ShouldInitializeWithDefaults()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var email = "test@example.com";
+            var hash = new byte[] { 1, 2, 3 };
+            var salt = new byte[] { 4, 5, 6 };
+            var userId = Guid.NewGuid();
+
+            // Act
+            var account = new Account(id, email, hash, salt, userId);
+
+            // Assert
+            Assert.Equal(id, account.Id);
+            Assert.Equal(email, account.Email);
+            Assert.Equal(hash, account.HashPassword);
+            Assert.Equal(salt, account.Salt);
+            Assert.Equal(userId, account.UserId);
+            Assert.True(account.IsActive);
+            Assert.False(account.IsDeleted);
+        }
+
+        [Fact]
+        public void Deactivate_ShouldSetIsActiveFalse()
+        {
+            var account = new Account(Guid.NewGuid(), "a@b.com", new byte[1], new byte[1], Guid.NewGuid());
+
+            account.Deactivate();
+
+            Assert.False(account.IsActive);
+        }
+
+        [Fact]
+        public void Delete_ShouldSetIsDeletedTrue()
+        {
+            var account = new Account(Guid.NewGuid(), "a@b.com", new byte[1], new byte[1], Guid.NewGuid());
+
+            account.Delete();
+
+            Assert.True(account.IsDeleted);
+        }
+    }
+}
---
+++ ControlHub/tests/ControlHub.Application.Tests/Entities/UserTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ControlHub.Domain.Entities;
+using Xunit;
+
+namespace ControlHub.Domain.Tests.Entities
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Constructor_ShouldInitializeWithDefaults()
+        {
+            var id = Guid.NewGuid();
+            var accId = Guid.NewGuid();
+
+            var user = new User(id, accId, "john");
+
+            Assert.Equal(id, user.Id);
+            Assert.Equal(accId, user.AccId);
+            Assert.Equal("john", user.Username);
+            Assert.False(user.IsDeleted);
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrow_WhenIdIsEmpty()
+        {
+            Assert.Throws<ArgumentException>(() => new User(Guid.Empty, Guid.NewGuid(), "john"));
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrow_WhenAccountIdIsEmpty()
+        {
+            Assert.Throws<ArgumentException>(() => new User(Guid.NewGuid(), Guid.Empty, "john"));
+        }
+
+        [Fact]
+        public void Delete_ShouldSetIsDeletedTrue()
+        {
+            var user = new User(Guid.NewGuid(), Guid.NewGuid(), "john");
+
+            user.Delete();
+
+            Assert.True(user.IsDeleted);
+        }
+
+        [Fact]
+        public void SetUsername_ShouldUpdateUsername_WhenValid()
+        {
+            var user = new User(Guid.NewGuid(), Guid.NewGuid());
+
+            user.SetUsername("newname");
+
+            Assert.Equal("newname", user.Username);
+        }
+
+        [Fact]
+        public void SetUsername_ShouldThrow_WhenEmpty()
+        {
+            var user = new User(Guid.NewGuid(), Guid.NewGuid());
+
+            Assert.Throws<ArgumentException>(() => user.SetUsername("  "));
+        }
+    }
+}
